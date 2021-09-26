ebdf23c678847a2a354b8b6e09973ebd96f6cec8
99061142 <99061142@mydavinci.nl>
2021-09-26 19:24:24 +0200

    Code + image of the code

---
+++ person.py
@@ -0,0 +1,20 @@
+print("Naaminvoer:")
+name = input("Wat is jou naam?: ")
+
+print("Adres:")
+address = input("Wat is jou adres?: ")
+
+print("Postcode:")
+zip_Code = input("Wat is jou postcode?: ")
+
+print("Woonplaats:")
+city = input("Wat is jou woonplaats?: ")
+
+print(
+    "\n ----------------------------------------------------",
+    "\n |  Naam      : ", name, 
+    "\n |  Adres     : ", address,          
+    "\n |  Postcode  : ", zip_Code,                     
+    "\n |  Woonplaats: ", city,                           
+    "\n ----------------------------------------------------"
+)
